df739b6eac2a6b99bee1f4c1e57d94d906a79afd
Matt Mullins <mmullins@mmlx.us>
2021-05-18 16:49:44 -0700

    firmware: blink LED1 at 2Hz
    It works!

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -3,7 +3,29 @@
 
 use panic_halt as _;
 
+use atsamd_hal::prelude::*;
+
 #[cortex_m_rt::entry]
 fn main() -> ! {
-    loop {}
+    let mut peripherals = atsamd_hal::target_device::Peripherals::take().unwrap();
+    let core_peripherals = atsamd_hal::target_device::CorePeripherals::take().unwrap();
+
+    let mut clock = atsamd_hal::clock::GenericClockController::with_internal_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.MCLK,
+        &mut peripherals.OSC32KCTRL,
+        &mut peripherals.OSCCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+    let mut delay = atsamd_hal::delay::Delay::new(core_peripherals.SYST, &mut clock);
+
+    let pins = atsamd_hal::gpio::v2::Pins::new(peripherals.PORT);
+    let mut led1 = pins.pb12.into_push_pull_output();
+
+    loop {
+        delay.delay_ms(250u16);
+        led1.set_high().unwrap();
+        delay.delay_ms(250u16);
+        led1.set_low().unwrap();
+    }
 }
